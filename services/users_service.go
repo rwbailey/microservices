f3a867f2bed93f3347dbee7319f4a860b3493b94
Richard Bailey <richard@richardbailey.xyz>
2020-10-08 00:21:02 +0100

    userDaoInterface userDaoMock

--- services/users_service.go
+++ services/users_service.go
@@ -12,5 +12,9 @@ var (
 )
 
 func (*usersService) GetUser(userId int64) (*domain.User, *utils.ApplicationError) {
-	return domain.UserDao.GetUser(userId)
+	user, err := domain.UserDao.GetUser(userId)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
---
+++ services/users_service_test.go
@@ -0,0 +1,58 @@
+package services
+
+import (
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/rwbailey/microservices/domain"
+	"github.com/rwbailey/microservices/utils"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type userDaoMock struct{}
+
+var (
+	getUserFunction func(userId int64) (*domain.User, *utils.ApplicationError)
+)
+
+func init() {
+	domain.UserDao = &userDaoMock{}
+}
+
+func (*userDaoMock) GetUser(userId int64) (*domain.User, *utils.ApplicationError) {
+	log.Println("testing with mock data")
+	return getUserFunction(123)
+}
+
+func TestGetUserNotFoundInDatebase(t *testing.T) {
+	getUserFunction = func(userId int64) (*domain.User, *utils.ApplicationError) {
+		return nil, &utils.ApplicationError{
+			Message:    "user 0 was not found",
+			StatusCode: http.StatusNotFound,
+		}
+	}
+
+	user, err := UsersService.GetUser(0)
+
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "user 0 was not found", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	getUserFunction = func(userId int64) (*domain.User, *utils.ApplicationError) {
+		return &domain.User{
+				Id: 123,
+			},
+			nil
+	}
+
+	user, err := UsersService.GetUser(123)
+
+	assert.NotNil(t, user)
+	assert.Nil(t, err)
+	assert.EqualValues(t, 123, user.Id)
+}
