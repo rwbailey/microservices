d8efdf088b0d9aab56548cbd3ead9e4b66d6d7f4
Richard Bailey <richard@richardbailey.xyz>
2020-10-07 22:41:14 +0100

    TestGetUserNoError

--- domain/user_dao_test.go
+++ domain/user_dao_test.go
@@ -16,3 +16,15 @@ func TestGetUserNoUserFound(t *testing.T) {
 	assert.EqualValues(t, "not found", err.Code)
 	assert.EqualValues(t, "user 0 was not found", err.Message)
 }
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	assert.EqualValues(t, 123, user.Id)
+	assert.EqualValues(t, "Richard", user.FirstName)
+	assert.EqualValues(t, "Bailey", user.LastName)
+	assert.EqualValues(t, "richard@example.com", user.Email)
+}
