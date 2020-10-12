38faab09b4cbe775d9521f9e5891f6ebaa3d4049
Richard Bailey <richard@richardbailey.xyz>
2020-10-12 18:17:56 +0100

    added src folders

---
+++ src/api/domain/github/create_repo.go
@@ -0,0 +1,13 @@
+package github
+
+/*
+{
+    "name": "Microservices Github API",
+    "description": "Github API integration",
+    "homepage": "https://github.com",
+    "private": false,
+    "has_issues": true,
+    "has_projects": true,
+    "has_wiki": true
+}
+*/
