3cb70be0e49a21fb08c4562b8d1bd66a675b7a72
Richard Bailey <richard@richardbailey.xyz>
2020-10-12 20:25:12 +0100

    json response model

--- src/api/domain/github/create_repo.go
+++ src/api/domain/github/create_repo.go
@@ -1,17 +1,5 @@
 package github
 
-/*
-	{
-		"name": "Microservices Github API",
-		"description": "Github API integration",
-		"homepage": "https://github.com",
-		"private": false,
-		"has_issues": true,
-		"has_projects": true,
-		"has_wiki": true
-	}
-*/
-
 type CreateRepoRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
@@ -20,4 +8,25 @@ type CreateRepoRequest struct {
 	HasIssues   bool   `json:"has_issues"`
 	HasProjects bool   `json:"has_projects"`
 	HasWiki     bool   `json:"has_wiki"`
+}
+
+type CreateRepoResponse struct {
+	Id          int64           `json:"id"`
+	Name        string          `json:"name"`
+	FullName    string          `json:"full_name"`
+	Owner       RepoOwner       `json:"owner"`
+	Permissions RepoPermissions `json:"permissions"`
+}
+
+type RepoOwner struct {
+	Id      int64  `json:"id"`
+	Login   string `json:"login"`
+	Url     string `json:"url"`
+	HtmlUrl string `json:"html_url"`
+}
+
+type RepoPermissions struct {
+	IsAdmin bool `json:"admin"`
+	HasPush bool `json:"push"`
+	HasPull bool `json:"pull"`
 }
--- src/api/domain/github/create_repo_test.go
+++ src/api/domain/github/create_repo_test.go
@@ -21,9 +21,16 @@ func TestCreateRepoRequestAsJson(t *testing.T) {
 	bytes, err := json.Marshal(request)
 	assert.Nil(t, err)
 	assert.NotNil(t, bytes)
-	// assert.EqualValues(t, string(bytes), `{"name":"test-repo","description":"This is a Test Repo Created Via The API.","homepage":"https://github.com","private":true,"has_issues":true,"has_projects":true,"has_wiki":true}`)
 
 	var target CreateRepoRequest
 	err = json.Unmarshal(bytes, &target)
 	assert.Nil(t, err)
+
+	assert.EqualValues(t, target.Name, request.Name)
+	assert.EqualValues(t, target.Description, request.Description)
+	assert.EqualValues(t, target.Homepage, request.Homepage)
+	assert.EqualValues(t, target.Private, request.Private)
+	assert.EqualValues(t, target.HasIssues, request.HasIssues)
+	assert.EqualValues(t, target.HasProjects, request.HasProjects)
+	assert.EqualValues(t, target.HasWiki, request.HasWiki)
 }
