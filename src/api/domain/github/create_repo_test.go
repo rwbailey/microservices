89201447eec982748c997598873d7029cc77d787
Richard Bailey <richard@richardbailey.xyz>
2020-10-12 19:57:39 +0100

    define json

--- src/api/domain/github/create_repo_test.go
+++ src/api/domain/github/create_repo_test.go
@@ -1,11 +1,26 @@
 package github
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateRepoRequestAsJson(t *testing.T) {
-	assert.New()
+	request := CreateRepoRequest{
+		Name:        "test-repo",
+		Description: "This is a Test Repo Created Via The API.",
+		Homepage:    "https://github.com",
+		Private:     true,
+		HasIssues:   true,
+		HasProjects: true,
+		HasWiki:     true,
+	}
+
+	bytes, err := json.Marshal(request)
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+	fmt.Println(string(bytes))
 }
